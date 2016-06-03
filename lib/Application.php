0326186bd67c3fbfc13682dfd3d4382baf511bfb
Mátyás Somfai <somfai.matyas@gmail.com>
2016-06-03 10:40:47 +0200

    framework bootstrap with autoloading

---
+++ lib/Application.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of the mfw package.
+ *
+ * (c) Mátyás Somfai <somfai.matyas@gmail.com>
+ * Created at 2016.06.03.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Smatyas\Mfw;
+
+/**
+ * An mfw application.
+ */
+class Application
+{
+    /**
+     * Runs the application.
+     */
+    public function run()
+    {
+        phpinfo();
+    }
+}
--- web/index.php
+++ web/index.php
@@ -1,2 +1,13 @@
-<?php 
-phpinfo();
+<?php
+
+$autoloader = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($autoloader)) {
+    echo 'Missing autoloader file. Run `composer install` to install composer dependencies.';
+    exit(1);
+}
+require $autoloader;
+
+use Smatyas\Mfw\Application;
+
+$app = new Application();
+$app->run();
