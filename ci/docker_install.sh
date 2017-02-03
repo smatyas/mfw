2956da9af66900e0826867dbd4ca880a309dd32d
Mátyás Somfai <somfai.matyas@gmail.com>
2017-02-03 22:04:54 +0100

    code coverage report

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -8,5 +8,6 @@ set -xe
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -y
 apt-get install -y git wget zip unzip mysql-client libicu-dev zlib1g-dev libmemcached-dev libpng-dev libfreetype6-dev libjpeg62-turbo-dev
+pecl install xdebug
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-install pdo_mysql opcache gd
